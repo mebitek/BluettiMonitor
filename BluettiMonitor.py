1597f8cb91fd3f101cbadf1fd474cd2f94989d7e
mebitek <mebitek@zohomail.eu>
2026-03-11 10:24:41 +0100

    add detention time

--- BluettiMonitor.py
+++ BluettiMonitor.py
@@ -245,6 +245,8 @@ class BluettiMonitorService:
             return GenericReg.OK.value, utils.convert_decimal(13.6)
         elif reg_id == BluettiReg.VE_REG_PEUKERT_COEFFICIENT.value:
             return GenericReg.OK.value, utils.convert_decimal(1.01)
+        elif reg_id == BluettiReg.VE_REG_CHARGE_DETECTION_TIME.value:
+            return GenericReg.OK.value, utils.convert_decimal(3)
 
         else:
             logging.debug("GET REG_ID %s" % regid)
