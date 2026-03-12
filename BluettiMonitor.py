06b4b005713ee3d9d5f2a41c69e87e40c2cc403d
mebitek <mebitek@zohomail.eu>
2026-03-12 17:00:29 +0100

    discharge threshold

--- BluettiMonitor.py
+++ BluettiMonitor.py
@@ -304,11 +304,9 @@ class BluettiMonitorService:
         elif reg_id == BluettiReg.VE_REG_CHARGED_CURRENT.value:
             return GenericReg.OK.value, utils.convert_decimal(0.02) #tail current
         elif reg_id == BluettiReg.VE_REG_LOW_SOC.value:
-            return GenericReg.OK.value, utils.convert_decimal(self.config.get_low_soc_alarm_set())
-        elif reg_id == BluettiReg.VE_REG_LOW_SOC_CLEAR.value:
-            return GenericReg.OK.value, utils.convert_decimal(self.config.get_low_soc_alarm_clear())
-        # elif reg_id == BluettiReg.VE_REG_HIST_LAST_DISCHARGE.value:
-        #     return GenericReg.OK.value, utils.convert_decimal(self.bluetti.hist_last_discharge)
+            return GenericReg.OK.value, utils.convert_decimal(0.1) #discharge threshold
+        elif reg_id == BluettiReg.VE_REG_HIST_LAST_DISCHARGE.value:
+            return GenericReg.OK.value, utils.convert_decimal(self.bluetti.hist_last_discharge)
 
         else:
             logging.debug("GET REG_ID %s" % reg_id)
